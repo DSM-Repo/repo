bfa49cd9e4ebb1ecf280debe9892760d51e9fbc2
six-standard <dbrrl1127@gmail.com>
2024-06-25 23:35:34 +0900

    feat: 일부 페이지 기능 추가

--- packages/main/src/components/header.tsx
+++ packages/main/src/components/header.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import Logo from "../assets/Logo.svg?react";
 
 export const Header = () => {
   return (
     <div className="flex items-center justify-center w-full bg-[#1B1B1B] py-3 fixed top-0 left-0 z-30">
-      <Logo width="72px" height="22px" />
-      <span className="cursor-pointer absolute right-48 text-[#ffffff] font-bold hover:text-[#c5c5c5]">
+      <Logo
+        width="72px"
+        height="22px"
+        className="cursor-pointer"
+        onClick={() => window.location.replace("http://localhost:5173/")}
+        //student, teacher 등 url이 다른 사이트에서도 홈에 쉽게 접속할 수 있게 window 방식으로 작성
+      />
+      <span className="pointable absolute right-48 text-[#ffffff] font-bold">
         Login →
       </span>
     </div>
--- packages/main/src/pages/Landing.tsx
+++ packages/main/src/pages/Landing.tsx
@@ -16,10 +16,10 @@ export const Landing = () => {
         <div className="w-[75%] flex gap-10 items-center justify-between">
           <div className="flex flex-col gap-8">
             <Logo width="200px" height="70px" />
-            <span className="text-[24px] mt-[-6px] text-white font-thin">
+            <span className="text-[24px] mt-[-6px] text-white font-thin line-fit">
               이력서 온라인 작성 서비스
             </span>
-            <button className="text-white w-[fit-content] px-4 py-2 border-2 border-white rounded-[2px]">
+            <button className="text-white w-[fit-content] px-4 py-2 border-2 border-white rounded-[2px] pointable">
               Repo 시작하기
             </button>
           </div>
@@ -28,12 +28,14 @@ export const Landing = () => {
         <div className="w-full h-[200px] rotate-180 absolute bottom-0 bg-[#1B1B1B] z-10" />
       </div>
       <div className="flex items-center justify-between w-[75%] h-[100vh]">
-        <div className="flex flex-col gap-2">
-          <span className="text-white font-semibold text-[45px]">간편한</span>
-          <span className="text-[#C5C5C5] font-semibold text-[30px]">
+        <div className="flex flex-col gap-6">
+          <span className="text-white font-semibold text-[45px] line-fit">
+            간편한
+          </span>
+          <span className="text-[#C5C5C5] font-semibold text-[30px] line-fit">
             정보 입력
           </span>
-          <span className="w-[240px] text-white text-[20px] font-thin">
+          <span className="w-[240px] text-white text-[20px] font-thin line-fit-br">
             간편하게 정보들을 입력하고, 멋진 이력서를 생성해 보세요
           </span>
         </div>
@@ -41,23 +43,27 @@ export const Landing = () => {
       </div>
       <div className="flex items-center justify-between w-[75%] h-[100vh]">
         <div className="w-[650px] h-[450px] bg-white z-20 relative" />
-        <div className="flex flex-col gap-2">
-          <span className="text-white font-semibold text-[45px]">빠른</span>
-          <span className="text-[#C5C5C5] font-semibold text-[30px]">
+        <div className="flex flex-col gap-6">
+          <span className="text-white font-semibold text-[45px] line-fit">
+            빠른
+          </span>
+          <span className="text-[#C5C5C5] font-semibold text-[30px] line-fit">
             피드백 확인
           </span>
-          <span className="w-[325px] text-white text-[20px] font-thin">
+          <span className="w-[325px] text-white text-[20px] font-thin line-fit-br">
             선생님이 피드백 해주신 내용을 바탕으로 레주메를 수정해 보세요
           </span>
         </div>
       </div>
       <div className="relative flex items-center justify-center h-[95vh] w-[100vw]">
         <div className="flex flex-col items-center gap-2 absolute z-20">
-          <span className="text-white font-semibold text-[50px]">깔끔한</span>
-          <span className="text-white font-semibold text-[30px]">
+          <span className="text-white font-semibold text-[50px] line-fit">
+            깔끔한
+          </span>
+          <span className="text-white font-semibold text-[30px] line-fit">
             나만의 레주메
           </span>
-          <button className="mt-4 text-white w-[fit-content] px-4 py-2 border-2 border-white rounded-[2px]">
+          <button className="mt-4 text-white w-[fit-content] px-4 py-2 border-2 border-white rounded-[2px] pointable">
             Repo 시작하기
           </button>
         </div>
@@ -79,14 +85,12 @@ export const Landing = () => {
         </div>
         <div className="flex flex-col justify-between items-end h-full">
           <div className="flex gap-3">
-            <Facebook className="hover:opacity-70 cursor-pointer" />
-            <Github className="hover:opacity-70 cursor-pointer" />
+            <Facebook className="pointable" />
+            <Github className="pointable" />
           </div>
           <span className="text-white">
-            <a className="cursor-pointer hover:text-[#c5c5c5]">
-              개인정보처리방침
-            </a>{" "}
-            | <a className="cursor-pointer hover:text-[#c5c5c5]">이용약관</a>
+            <a className="pointable">개인정보처리방침</a> |{" "}
+            <a className="pointable">이용약관</a>
           </span>
         </div>
       </div>
