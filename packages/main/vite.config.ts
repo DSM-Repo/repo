0f60f56cc7c11fda5a5672e92a431b12bca0d991
six-standard <dbrrl1127@gmail.com>
2024-06-26 15:33:52 +0900

    chore: 절대경로 설정

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   cacheDir: "./.vite",
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
