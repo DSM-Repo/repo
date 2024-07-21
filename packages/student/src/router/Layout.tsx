46707e9cb8886bd7800eb7b96c27764fe22f736a
six-standard <dbrrl1127@gmail.com>
2024-07-21 23:58:28 +0900

    fix: 스타일링 관련 문제 해결

--- packages/student/src/router/Layout.tsx
+++ packages/student/src/router/Layout.tsx
@@ -6,7 +6,7 @@ import { Header } from "ui";
 export const Layout = () => {
   return (
     <div className="w-full h-[100vh] flex">
-      <SideBar user={user}>
+      <SideBar user={user} className="min-w-[250px]">
         <SideBarButton title="내 정보" icon="My" url="/" />
         <SideBarButton title="도서관" icon="Library" url="/library" />
         <SideBarDrop
@@ -23,7 +23,9 @@ export const Layout = () => {
       </SideBar>
       <div className="flex flex-col w-full h-full">
         <Header />
-        <Outlet />
+        <div className="w-full h-full overflow-scroll">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
