b07c347e2655f26ebb7e70ef640010e670c8bed1
six-standard <dbrrl1127@gmail.com>
2024-07-25 22:50:35 +0900

    chore: 데이터 로딩중 표시

--- packages/student/src/pages/Write/index.tsx
+++ packages/student/src/pages/Write/index.tsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import * as _ from "./components";
+import { Icon } from "@iconify/react";
 
 export const Write = () => {
   const section = parseInt(useParams().section as string);
+  const isLoading = false;
 
   if (section) {
     return (
-      <div className="flex w-full h-full">
+      <div className="flex w-full h-full relative">
         <div className="flex flex-col w-7/12 h-full overflow-x-hidden">
           <div
             className="flex w-full h-full transition-all duration-300 shrink"
@@ -23,6 +25,16 @@ export const Write = () => {
           <_.Footer section={section} />
         </div>
         <div className="w-5/12 h-full bg-black"></div>
+        {isLoading && (
+          <div className="w-full h-full col-flex flex-center gap-5 absolute z-20 bg-[#00000080]">
+            <Icon
+              icon="line-md:loading-twotone-loop"
+              color="white"
+              width={80}
+            />
+            <span className="text-body3">데이터를 불러오고 있습니다...</span>
+          </div>
+        )}
       </div>
     );
   }
