c515ebe3cde8fa685dc5fd3f6b581cbf6d19e989
six-standard <dbrrl1127@gmail.com>
2024-07-28 23:56:09 +0900

    chore: 로그인 페이지 연동 준비

--- packages/student/src/pages/Login.tsx
+++ packages/student/src/pages/Login.tsx
@@ -3,29 +3,29 @@ import { Button, Header, Input } from "ui";
 
 export const Login = () => {
   const [data, setData] = useState({
-    accountId: "",
+    account_id: "",
     password: "",
   });
 
   return (
-    <div className="flex w-full h-[100vh] relative flex-center">
+    <div className="flex w-full h-screen relative flex-center">
       <Header className="absolute top-0" />
       <div className="col-flex items-center gap-10">
         <span className="text-title1">로그인</span>
         <Input
+          value={data.account_id}
+          onChange={(e) => setData({ ...data, account_id: e.target.value })}
           label="아이디"
           placeholder="스퀘어 아이디를 입력하세요"
-          value={data.accountId}
-          onChange={(e) => setData({ ...data, accountId: e.target.value })}
           size="large"
         />
         <Input
-          password
-          label="비밀번호"
-          placeholder="비밀번호를 입력하세요"
           value={data.password}
           onChange={(e) => setData({ ...data, password: e.target.value })}
+          label="비밀번호"
+          placeholder="비밀번호를 입력하세요"
           size="large"
+          password
         />
         <Button onClick={() => {}} size="large">
           로그인
