13e03ad99ca8c57537594146ff58909b54248765
six-standard <dbrrl1127@gmail.com>
2024-08-19 14:18:47 +0900

    feat: startbutton 페이지 이동 추가

--- packages/main/src/pages/Landing/components/StartButton.tsx
+++ packages/main/src/pages/Landing/components/StartButton.tsx
@@ -1,6 +1,11 @@
 export const StartButton = () => {
   return (
-    <button className="w-fit px-4 py-3 text-body8 border-2 text-white border-white rounded-sm pointable leading-none">
+    <button
+      className="w-fit px-4 py-3 text-body8 border-2 text-white border-white rounded-sm pointable leading-none"
+      onClick={() =>
+        window.location.replace(`${process.env.VITE_APP_URL_STUDENT}/login`)
+      }
+    >
       Repo 시작하기
     </button>
   );
