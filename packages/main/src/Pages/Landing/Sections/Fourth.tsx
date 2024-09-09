b6f3ed0e28ccb7d99dc2576ed011eabbf76f685f
taeyoooon <leteyune101010@gmail.com>
2024-09-09 14:57:32 +0900

    위치 이상함 수정

--- packages/main/src/Pages/Landing/Sections/Fourth.tsx
+++ packages/main/src/Pages/Landing/Sections/Fourth.tsx
@@ -3,7 +3,7 @@ import { Title } from "./Title";
 export const Fourth = () => {
   return (
     <div className="flex items-center gap-9 w-full py-20">
-      <img src="/landing_list.png" className="w-[400px]" />
+      <img src="/landing_list.png" className="w-[400px] shadow-[0_0_40px_rgba(0,0,0,0.08)]" />
       <div className="col-flex gap-6">
         <Title direction="left">이력서 도서관</Title>
         <span className="text-[20px] font-light leading-none text-gray-50">
--- packages/main/src/Pages/Landing/Sections/Second.tsx
+++ packages/main/src/Pages/Landing/Sections/Second.tsx
@@ -4,9 +4,9 @@ export const Second = () => {
   return (
     <section className="col-flex gap-8 w-full py-20">
       <Title direction="center">손쉽게 작성하는 이력서</Title>
-      <div className="flex gap-6 w-full h-fit">
-        <div className="col-flex w-full">
-            <img src="/landing_award.png" className="w-[528px] h-[228px]" />
+      <div className="flex justify-center gap-6 w-fit h-fit">
+        <div className="col-flex w-fit mr-[24px]">
+          <img src="/landing_award.png" className="w-[528px] h-[228px]" />
           <span className="text-[24px] font-bold text-gray-50 mt-4">
             이력서 정보별 작성
           </span>
@@ -16,8 +16,8 @@ export const Second = () => {
             빠르게 오가며 채워나갈 수 있어요.
           </span>
         </div>
-        <div className="col-flex w-full">
-            <img src="/landing_nav.png" className="w-[528px] h-[228px]" />
+        <div className="col-flex w-fit">
+          <img src="/landing_nav.png" className="w-[528px] h-[228px]" />
           <span className="text-[24px] font-bold text-gray-50 mt-4">
             다양한 입력
           </span>
