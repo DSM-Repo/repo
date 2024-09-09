64e66039509a3a3e4d19b0536aba8a0c21693768
six-standard <dbrrl1127@gmail.com>
2024-09-09 16:02:02 +0900

    feat: 메인화면 접속시 토큰 있으면 이동하게 수정

--- packages/main/src/router/index.tsx
+++ packages/main/src/router/index.tsx
@@ -1,7 +1,19 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useAuth } from "@configs/util";
 import * as _ from "../Pages";
+import { useEffect } from "react";
 
 export const Router = () => {
+  const { getRole } = useAuth();
+  useEffect(() => {
+    const role = getRole();
+    if (role === "student") {
+      window.location.replace(process.env.VITE_APP_URL_STUDENT as string);
+    } else if (role === "teacher") {
+      window.location.replace(process.env.VITE_APP_URL_TEACHER as string);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
