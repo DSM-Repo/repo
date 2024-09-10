76b6cc6f8e89c2c5c7e4e6c4dea1a3979eb33f6a
six-standard <dbrrl1127@gmail.com>
2024-09-10 21:20:23 +0900

    refactor: 필요없는 파일 제거

--- packages/student/src/apis/auth.ts
+++
@@ -1,6 +0,0 @@
-import { useMyMutation } from "@configs/util";
-import { _ILogin, ILogin_ } from "./types";
-export * from "./types";
-
-export const login = () =>
-  useMyMutation<_ILogin, ILogin_>("post", "auth", "/student");
