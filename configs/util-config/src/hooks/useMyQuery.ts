749a2cbc871298f77314611c07b61f54d0f9ebc5
six-standard <dbrrl1127@gmail.com>
2024-09-10 21:20:57 +0900

    chore: useMyQuery 기본값 prop 추가

--- configs/util-config/src/hooks/useMyQuery.ts
+++ configs/util-config/src/hooks/useMyQuery.ts
@@ -4,13 +4,15 @@ import { instance, path, pathType } from "../apis";
 
 export const useMyQuery = <T>(
   pathname: pathType,
-  url: string
+  url: string,
+  placeholder: T
 ): UseQueryResult<T> => {
   return useQuery<T>({
     queryKey: [path[pathname], url],
     queryFn: async (): Promise<T> => {
       const res = await instance.get(path[pathname] + url);
       return res.data;
-    }
+    },
+    placeholderData: placeholder
   });
 };
