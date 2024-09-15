835098407e28bb598cf63623970bc6c21d5ba91f
six-standard <dbrrl1127@gmail.com>
2024-09-15 22:15:52 +0900

    fix: api ENUM 수정

--- packages/teacher/src/page/Resume.tsx
+++ packages/teacher/src/page/Resume.tsx
@@ -1,28 +1,27 @@
+import { Button, JSONViewer, Dropdown, TextArea } from "ui";
+import { AddFeedback, getFeedback } from "@/apis";
+import { findKeyWithValue } from "@configs/util";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useState } from "react";
 import {
   ConfirmFeedback,
   DeleteFeedback,
   RejectFeedback,
   studentDetail
 } from "@/apis";
-import { useMemo, useState } from "react";
-import { useParams } from "react-router-dom";
-import { findKeyWithValue } from "@configs/util";
-import { Button, Resume as _Resume, Dropdown, TextArea, Viewer } from "ui";
-import { AddFeedback, getFeedback } from "@/apis";
-import { toast } from "react-toastify";
 
 const typeTable: Record<string, string> = {
   "내 정보": "WRITER_INFO",
   자기소개: "INTRODUCE",
   프로젝트: "PROJECT",
-  "자격증 & 수상": "CERTIFICATE",
+  "자격증 & 수상": "ACHIEVEMENT",
   활동: "ACTIVITY"
 };
 
 export const Resume = () => {
   const { id } = useParams();
   const { data } = studentDetail(id as string);
-  const [file, setFile] = useState<File | undefined>(undefined);
   const { data: feedbacks, refetch: refetchFeedbacks } = getFeedback(
     id as string
   );
@@ -44,135 +43,127 @@ export const Resume = () => {
   };
 
   return (
-    <div className="w-full h-screen col-flex relative">
-      {/* <Viewer
-        url={file}
-        buttons={[
-          { icon: "Edit", title: "피드백 관리", action: "피드백 관리" }
-        ]}
-        sidebars={[
-          {
-            name: "피드백 관리",
-            type: "standard",
-            items: [
-              {
-                name: "피드백 작성",
-                content: (
-                  <div className="col-flex gap-2">
-                    <Dropdown
-                      placeholder="항목 선택"
-                      size="full"
-                      selected={findKeyWithValue(typeTable, feed.type)}
-                      id="type"
-                      onChange={(item, id) =>
-                        setFeed({ ...feed, [id as string]: typeTable[item] })
-                      }
-                      selections={Object.keys(typeTable)}
-                    />
-                    <TextArea
-                      rows={10}
-                      value={feed.comment}
-                      id="comment"
-                      onChange={(e) =>
-                        setFeed({
-                          ...feed,
-                          [e.target.id as string]: e.target.value
-                        })
-                      }
-                      size="full"
-                      placeholder="내용 입력"
-                    />
-                    <Button
-                      size="full"
-                      disabled={data?.status !== "SUBMITTED"}
-                      onClick={() =>
-                        add(feed, {
-                          onSuccess: () => {
-                            toast.success("성공적으로 추가되었습니다");
-                            setFeed(defaultFeed);
-                            refetchFeedbacks();
-                          },
-                          onError: () => console.log("err")
-                        })
-                      }
-                      direction="center"
-                    >
-                      {`피드백 추가 ${data?.status !== "SUBMITTED" ? "(미제출된 문서)" : ""}`}
-                    </Button>
-                  </div>
-                )
-              },
-              {
-                name: "피드백 목록",
-                content: (
-                  <div className="col-flex gap-2">
-                    {feedbacks?.data.length !== 0 ? (
-                      feedbacks?.data.map(
-                        (i) =>
-                          !i.rejected && (
-                            <div className="col-flex gap-1">
-                              <TextArea
-                                onChange={() => {}}
-                                placeholder=""
-                                size="full"
-                                value={i.comment}
-                                disabled
-                              />
-                              <div className="w-full flex items-center justify-between">
-                                <span className="text-[16px] text-light text-green-400">
-                                  {i.status === "CONFIRMED"
-                                    ? "반영된 피드백"
-                                    : ""}
-                                </span>
-                                <div className="flex gap-2 items-center">
-                                  {i.status === "CONFIRMED" ? (
-                                    <>
-                                      <span
-                                        className="text-[15px] cursor-pointer"
-                                        onClick={() =>
-                                          confirm(`?id=${i.id}`, { onSuccess })
-                                        }
-                                      >
-                                        수락
-                                      </span>
-                                      <span
-                                        className="text-[15px] cursor-pointer"
-                                        onClick={() =>
-                                          reject(`?id=${i.id}`, { onSuccess })
-                                        }
-                                      >
-                                        거절
-                                      </span>
-                                    </>
-                                  ) : (
+    <JSONViewer
+      data={data}
+      buttons={[{ icon: "Edit", title: "피드백 관리", action: "피드백 관리" }]}
+      sidebars={[
+        {
+          name: "피드백 관리",
+          type: "standard",
+          items: [
+            {
+              name: "피드백 작성",
+              content: (
+                <div className="col-flex gap-2">
+                  <Dropdown
+                    placeholder="항목 선택"
+                    size="full"
+                    selected={findKeyWithValue(typeTable, feed.type)}
+                    id="type"
+                    onChange={(item, id) =>
+                      setFeed({ ...feed, [id as string]: typeTable[item] })
+                    }
+                    selections={Object.keys(typeTable)}
+                  />
+                  <TextArea
+                    rows={10}
+                    value={feed.comment}
+                    id="comment"
+                    onChange={(e) =>
+                      setFeed({
+                        ...feed,
+                        [e.target.id as string]: e.target.value
+                      })
+                    }
+                    size="full"
+                    placeholder="내용 입력"
+                  />
+                  <Button
+                    size="full"
+                    disabled={data?.status !== "SUBMITTED"}
+                    onClick={() =>
+                      add(feed, {
+                        onSuccess: () => {
+                          toast.success("성공적으로 추가되었습니다");
+                          setFeed(defaultFeed);
+                          refetchFeedbacks();
+                        },
+                        onError: () => console.log("err")
+                      })
+                    }
+                    direction="center"
+                  >
+                    {`피드백 추가 ${data?.status !== "SUBMITTED" ? "(미제출된 문서)" : ""}`}
+                  </Button>
+                </div>
+              )
+            },
+            {
+              name: "피드백 목록",
+              content: (
+                <div className="col-flex gap-2">
+                  {feedbacks?.data.length !== 0 ? (
+                    feedbacks?.data.map(
+                      (i) =>
+                        !i.rejected && (
+                          <div className="col-flex gap-1">
+                            <TextArea
+                              onChange={() => {}}
+                              placeholder=""
+                              size="full"
+                              value={i.comment}
+                              disabled
+                            />
+                            <div className="w-full flex items-center justify-between">
+                              <span className="text-[16px] text-light text-green-400">
+                                {i.status === "CONFIRMED"
+                                  ? "반영된 피드백"
+                                  : ""}
+                              </span>
+                              <div className="flex gap-2 items-center">
+                                {i.status === "CONFIRMED" ? (
+                                  <>
                                     <span
                                       className="text-[15px] cursor-pointer"
                                       onClick={() =>
-                                        del(`/${i.id}`, { onSuccess })
+                                        confirm(`?id=${i.id}`, { onSuccess })
                                       }
                                     >
-                                      삭제
+                                      수락
                                     </span>
-                                  )}
-                                </div>
+                                    <span
+                                      className="text-[15px] cursor-pointer"
+                                      onClick={() =>
+                                        reject(`?id=${i.id}`, { onSuccess })
+                                      }
+                                    >
+                                      거절
+                                    </span>
+                                  </>
+                                ) : (
+                                  <span
+                                    className="text-[15px] cursor-pointer"
+                                    onClick={() =>
+                                      del(`/${i.id}`, { onSuccess })
+                                    }
+                                  >
+                                    삭제
+                                  </span>
+                                )}
                               </div>
                             </div>
-                          )
-                      )
-                    ) : (
-                      <span className="text-[14px]">
-                        아직 피드백이 없습니다
-                      </span>
-                    )}
-                  </div>
-                )
-              }
-            ]
-          }
-        ]}
-      /> */}
-      {/* {data && <ClientRenderer data={data} setter={setFile} />} */}
-      {data && <_Resume data={data} setMax={() => {}} showPadding />}
-    </div>
+                          </div>
+                        )
+                    )
+                  ) : (
+                    <span className="text-[14px]">아직 피드백이 없습니다</span>
+                  )}
+                </div>
+              )
+            }
+          ]
+        }
+      ]}
+    />
   );
 };
