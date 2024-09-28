f6655abd3c1b6b27471b3d3ccd94e2f55b561ae6
six-standard <dbrrl1127@gmail.com>
2024-09-28 18:35:27 +0900

    refactor: 공지 요소 분리

--- packages/student/src/pages/Notice/Item.tsx
+++ packages/student/src/pages/Notice/Item.tsx
@@ -1,50 +1,58 @@
-// import { Box, Icon } from "ui";
+import { Api } from "@configs/type";
+import { useRef } from "react";
+import { Box, Icon } from "ui";
 
-// interface IProp {
-//   data:
-// }
+interface IProp {
+  data: Api.Notice.noticeData;
+  opened: string | null;
+  actions: {
+    set: () => void;
+    check: () => void;
+  };
+}
 
-// export const Item = ({data}: IProp) => {
-//   return <Box
-//     height={opened === i.id ? height.current[i.id] + "px" : "60px"}
-//     round="12px"
-//     padding="16px"
-//     className="overflow-hidden gap-4 cursor-pointer transition-all duration-300"
-//     onClick={() => {
-//     setOpened(opened === i.id ? null : i.id);
-//     console.log(i.id);
-//     if (i.checked === false) {
-//       check(`/${i.id}`, { onSuccess: () => refetch() });
-//     }
-//   }}
-//   ref={(item: HTMLElement) => {
-//     if (item && !!!height.current[i.id]) {
-//       height.current[i.id] =
-//         (item.childNodes[1] as HTMLElement).clientHeight + 76;
-//     }
-//   }}
-// >
-//   <div className="flex justify-between items-center">
-//     <div className="flex items-center gap-3">
-//       <span className="text-body1">{i.title}</span>
-//       <span className="text-body4 text-gray-200">
-//         {i.created_at.split("T")[0]} |{" "}
-//         {i.writer_name === "육기준" || i.writer_name === "이태윤"
-//           ? `${i.writer_name}`
-//           : `${i.writer_name} 선생님`}
-//       </span>
-//       {!i.checked && (
-//         <span className="text-body5 text-green-400">안 읽음</span>
-//       )}
-//     </div>
-//     <Icon
-//       name="Arrow"
-//       rotate={opened === i.id ? "up" : "down"}
-//       size={28}
-//     />
-//   </div>
-//   <span className="break-words whitespace-pre-wrap">
-//     {i.content}
-//   </span>
-// </Box>;
-// };
+export const Item = ({ data, opened, actions }: IProp) => {
+  const height = useRef(0);
+
+  return (
+    <Box
+      height={opened === data.id ? height.current + "px" : "60px"}
+      round="12px"
+      padding="16px"
+      className="overflow-hidden gap-4 cursor-pointer transition-all duration-300"
+      onClick={() => {
+        actions.set();
+        if (data.checked === false) {
+          actions.check();
+        }
+      }}
+      ref={(item: HTMLElement) => {
+        if (item && !!!height.current) {
+          height.current =
+            (item.childNodes[1] as HTMLElement).clientHeight + 76;
+        }
+      }}
+    >
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-3">
+          <span className="text-body1">{data.title}</span>
+          <span className="text-body4 text-gray-200">
+            {data.created_at.split("T")[0]} |{" "}
+            {data.writer_name === "육기준" || data.writer_name === "이태윤"
+              ? `${data.writer_name}`
+              : `${data.writer_name} 선생님`}
+          </span>
+          {!data.checked && (
+            <span className="text-body5 text-green-400">안 읽음</span>
+          )}
+        </div>
+        <Icon
+          name="Arrow"
+          rotate={opened === data.id ? "up" : "down"}
+          size={28}
+        />
+      </div>
+      <span className="break-words whitespace-pre-wrap">{data.content}</span>
+    </Box>
+  );
+};
--- packages/student/src/pages/Notice/index.tsx
+++ packages/student/src/pages/Notice/index.tsx
@@ -1,10 +1,10 @@
 import { noticeCheck, noticeList } from "@configs/api";
-import { Layout, Title, Box, Icon } from "ui";
-import { useRef, useState } from "react";
+import { Layout, Title } from "ui";
+import { useState } from "react";
+import { Item } from "./Item";
 
 export const Notice = () => {
   const [opened, setOpened] = useState<string | null>(null);
-  const height = useRef<Record<string, number>>({});
   const { data, refetch } = noticeList();
   const { mutate: check } = noticeCheck();
 
@@ -21,50 +21,15 @@ export const Notice = () => {
               new Date(j.created_at).getTime() -
               new Date(i.created_at).getTime()
           )
-          .map((i, j) => (
-            <Box
-              key={j}
-              height={opened === i.id ? height.current[i.id] + "px" : "60px"}
-              round="12px"
-              padding="16px"
-              className="overflow-hidden gap-4 cursor-pointer transition-all duration-300"
-              onClick={() => {
-                setOpened(opened === i.id ? null : i.id);
-                console.log(i.id);
-                if (i.checked === false) {
-                  check(`/${i.id}`, { onSuccess: () => refetch() });
-                }
+          .map((i) => (
+            <Item
+              data={i}
+              opened={opened}
+              actions={{
+                set: () => setOpened(i.id),
+                check: () => check(`/${i.id}`, { onSuccess: () => refetch() })
               }}
-              ref={(item: HTMLElement) => {
-                if (item && !!!height.current[i.id]) {
-                  height.current[i.id] =
-                    (item.childNodes[1] as HTMLElement).clientHeight + 76;
-                }
-              }}
-            >
-              <div className="flex justify-between items-center">
-                <div className="flex items-center gap-3">
-                  <span className="text-body1">{i.title}</span>
-                  <span className="text-body4 text-gray-200">
-                    {i.created_at.split("T")[0]} |{" "}
-                    {i.writer_name === "육기준" || i.writer_name === "이태윤"
-                      ? `${i.writer_name}`
-                      : `${i.writer_name} 선생님`}
-                  </span>
-                  {!i.checked && (
-                    <span className="text-body5 text-green-400">안 읽음</span>
-                  )}
-                </div>
-                <Icon
-                  name="Arrow"
-                  rotate={opened === i.id ? "up" : "down"}
-                  size={28}
-                />
-              </div>
-              <span className="break-words whitespace-pre-wrap">
-                {i.content}
-              </span>
-            </Box>
+            />
           ))}
       </div>
     </Layout>
