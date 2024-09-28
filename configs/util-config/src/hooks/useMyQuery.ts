f59e45c460a77256de788e30591ef90308fbe6f7
six-standard <dbrrl1127@gmail.com>
2024-09-28 15:54:33 +0900

    chore: suspense 적용

--- configs/util-config/src/hooks/useMyQuery.ts
+++ configs/util-config/src/hooks/useMyQuery.ts
@@ -1,5 +1,6 @@
+// prettier-ignore
 // @ts-expect-error
-import { UseQueryResult, useQuery, keepPreviousData } from "@tanstack/react-query";
+import { UseQueryResult, useSuspenseQuery, keepPreviousData } from "@tanstack/react-query";
 import { instance, path, pathType } from "../apis";
 
 export const useMyQuery = <T>(
@@ -7,7 +8,7 @@ export const useMyQuery = <T>(
   url: string,
   placeholder: T
 ): UseQueryResult<T> => {
-  return useQuery<T>({
+  return useSuspenseQuery<T>({
     queryKey: [path[pathname], url],
     queryFn: async (): Promise<T> => {
       const res = await instance.get(path[pathname] + url);
