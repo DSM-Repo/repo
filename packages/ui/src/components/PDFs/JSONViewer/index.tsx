e7c090fac632bc5e35c3bafd27bc2091d96ace1c
six-standard <dbrrl1127@gmail.com>
2024-11-03 14:19:07 +0900

    chore: 복사 버튼 이동

--- packages/ui/src/components/PDFs/JSONViewer/index.tsx
+++ packages/ui/src/components/PDFs/JSONViewer/index.tsx
@@ -131,18 +131,6 @@ export const JSONViewer = ({
             });
           }
         },
-        {
-          icon: "Share",
-          title: "URL 복사",
-          action: () => {
-            navigator.clipboard
-              .writeText(
-                `${process.env.VITE_APP_URL_MAIN}/resume_viewer/${data?.id}`
-              )
-              .then(() => toast.success("성공적으로 복사되었습니다"))
-              .catch(() => toast.error("복사중 오류가 발생했습니다"));
-          }
-        },
         ...buttons
       ]}
       sidebars={sidebars}
